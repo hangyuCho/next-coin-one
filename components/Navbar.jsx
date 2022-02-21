36094700035f75f9aff5deb214bc1e8d9a86a5f0
hangyuCho <positive@hangyuChoui-MacBookAir.local>
2022-02-21 23:32:24 +0900

    fix nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,42 +1,41 @@
 import styles from "../styles/Navbar.module.css"
+import Image from "next/image"
+import LogoImage from "../public/img/coinone_logo_blue.svg"
+import ticketImageLager from "../public/img/gnb-banner-20210901.png"
+import ticketImageSmall from "../public/img/gnb-banner-icon-20210901.png"
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.items}>
-        <div className={styles.item}>
-          <div className={styles.logo}></div>
+      <div className={styles.left}>
+        <div className={styles.leftLogo}>
+          <LogoImage/>
         </div>
-        <div className={styles.item}>
-          <ul className={styles.list}>
-            <li className={styles}> <button>간편구매</button> </li>
-            <li className={styles}> <button>거래소</button> </li>
-            <li className={styles}> <button>프로차트</button> </li>
-            <li className={styles}> <button>자산</button> </li>
-            <li className={styles}> <button>코인정보</button> </li>
-            <li className={styles}> <button>플러스</button> </li>
-          </ul>
-        </div>
-        <div className={styles.item}>
-          <div className={styles}>
-            <div className={styles}>Ticket</div>
-            <div className={styles}>Notice</div>
-            <div className={styles}>
-              <button>회원가입</button>
-              <button>로그인</button>
-              <select>
-                <option>KR</option>
-              </select>
-            </div>
+      </div>
+      <div className={styles.center}>
+        <ul className={styles.centerList}>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>간편구매</button> </li>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>거래소</button> </li>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>프로차트</button> </li>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>자산</button> </li>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>코인정보</button> </li>
+          <li> <button className={[styles.btn, styles.centerListBtn].join(" ")}>플러스</button> </li>
+        </ul>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.rightMenu}>
+          <div className={styles.rightTicket}>
+            <Image src={ticketImageLager} alt=""/>
+            <Image src={ticketImageSmall} alt=""/>
           </div>
-        </div>
-        <div className={styles.item}>
-        </div>
-        <div className={styles.item}>
-        </div>
-        <div className={styles.item}>
-        </div>
-        <div className={styles.item}>
+          <div className={styles.rightNotice}>Notice</div>
+          <div className={styles.rightBtnGroup}>
+            <button className={[styles.btn, styles.rightSignInBtn].join(" ")}>회원가입</button>
+            <button className={[styles.btn, styles.rightLoginBtn].join(" ")}>로그인</button>
+            <select className={[styles.select, styles.rightSelectLanguage].join(" ")}>
+              <option>KR</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
